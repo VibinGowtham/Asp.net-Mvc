b0a0ebf376289224e0615645736dc48923414f8a
VibinGowtham <vibingowtham.cs18@bitsathy.ac.in>
2022-07-13 21:46:36 +0530

    Add project files.

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("vibin")]
+    public class TestController : Controller
+    {
+        [Route("home")]
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        [Route("display")]
+        public IActionResult Display()
+        {
+            List<Student> students = new List<Student>();
+            Student student1 = new Student("Vibin",1001,20,2017);
+            Student student2 = new Student("Gowtham",1002,21,2018);
+            Student student3 = new Student("Raj",1003,22,2022);
+            Student student4 = new Student("Rahul",1004,23,2021);
+            students.Add(student1);
+            students.Add(student2);
+            students.Add(student3);
+            students.Add(student4);
+            return View(students);
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public int Batch { get; set; }
+        public Student(string? name, int id, int age, int batch)
+        {
+            Name = name;
+            Id = id;
+            Age = age;
+            Batch = batch;
+        }
+
+
+    }
+}
